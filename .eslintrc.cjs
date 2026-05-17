d53d9a0c172d9d95f94f5ec866a4aeab3e3a1dbf
frankle1402 <lizhigao@gmail.com>
2026-05-17 17:44:54 +0800

    chore(agent-g): add ts/eslint/prettier base configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: { node: true, browser: true, es2022: true },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  ignorePatterns: ['dist', 'build', 'node_modules', 'coverage'],
+};
